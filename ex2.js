92c509a9573e62873d39380f6d69abbb731ac532
emilly <emilly.oliveira18@escola.pr.gov.br>
2026-05-14 11:35:43 -0300

    atividade

---
+++ ex2.js
@@ -0,0 +1,9 @@
+let num1 = input.question("Digite o  primeiro número: ")
+let num2 = input.question("Digite o  segundo número: ")
+
+console.log('soma: ${num1 + num2}')
+console.log('subtração: ${num1 - num2}')
+console.log(' divisão ${num1 / num2}')
+console.log(' multiplicação: ${num1 * num2}')
+
+if 
