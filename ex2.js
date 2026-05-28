12572bc0aa59d24663f6d534eec3959ace474bfe
emilly <emilly.oliveira18@escola.pr.gov.br>
2026-05-28 11:20:42 -0300

    atividade

--- ex2.js
+++ ex2.js
@@ -6,4 +6,11 @@ let num2 = Number(input.question("Digite o segundo número: "))
 console.log(`soma: ${num1 + num2}`)
 console.log(`subtração: ${num1 - num2}`)
 console.log(`multiplicação: ${num1 * num2}`)
-console.log(`divisão: ${num1 / num2}`)
+ 
+if(num2 === 0){
+    console.log(`Deu erro!`)
+}
+
+else{
+    console.log(`divisão: ${num1 / num2}`)
+}
