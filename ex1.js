25bf105f3c80a8b4fe43c716c00440662604fbf8
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-01 11:52:19 -0300

    up

--- ex1.js
+++
@@ -1,7 +0,0 @@
-const input = require('readline-sync')
-
-let nome = input.question("Digite seu nome ")
-let idade = input.question("Digite sua idade ")
-let cidade = input.question("Digite sua cidade ")
-
-console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}`)
