0fbe51e9c7beeed46f2dd540bb5300d7986b85ac
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-01 11:41:03 -0300

    aula01

--- ex3.js
+++ ex3.js
@@ -8,8 +8,16 @@ for(let i = 4; i >= 0;){
     soma = soma + numeros;
     media = soma / 5
     i--;
+
 }
-console.log(`${soma}`);
-console.log(`${media}`);
+console.log(`Sua média é de: ${media}`)
 
-
+if(media >= 70.0){
+    console.log(`Aprovado!`)
+}
+else if(media < 70.0){
+    console.log(`Em recuperação!`)
+}
+else if (media <= 50.0 ){
+    console.log(`Reprovado!`)
+}
