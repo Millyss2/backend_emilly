1e0fffa789bb7d55ba9f73f1914264aff04d45bf
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-01 12:21:09 -0300

    aula01

---
+++ aula01/ex5.js
@@ -0,0 +1,15 @@
+const input = require('readline-sync')
+
+let impar = 0
+let par = 0
+let num = 0
+
+
+for (let i = 0; i <= 10; i++)
+    numeros = Number(input.question(`escreva 10 números: `))
+
+if(numeros % 2 === 0){
+
+    pares
+}
+
