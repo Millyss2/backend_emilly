c64f8bc78119a26690f050bb7fe42c9c44d89779
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-11 08:08:05 -0300

    up

--- lista01-diego/ex2.js
+++ lista01-diego/ex2.js
@@ -6,6 +6,11 @@ let base = input.question("Escreva o tamanho da base desse triângulo: ")
 let area = 0
 let per = 0
 
+area = base * area / 2
+per = 2 * base + 2* altura
+
+console.log(`A área do triangulo é: ${area}`)
+console.log(`O perimetro do triangulo é: ${per}`)
 
 
 
