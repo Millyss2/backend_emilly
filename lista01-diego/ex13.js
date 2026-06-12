ae11c58f7c1f6251b50cc665a2a55331fa65cc89
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-12 12:02:16 -0300

    up

--- lista01-diego/ex13.js
+++ lista01-diego/ex13.js
@@ -1,1 +1,19 @@
 const input = require('readline-sync')
+
+function range(inicio, fim) {
+    const resultado = [];
+    for (let i = inicio; i <= fim; i++) {
+        resultado.push(i);
+    }
+    return resultado;
+}
+
+function soma(array) {
+    let total = 0;
+    for (let numero of array) {
+        total += numero;
+    }
+    return total;
+}
+
+console.log(soma(range(1, 10))); 
