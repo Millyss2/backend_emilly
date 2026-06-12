af0001fd28b081d85003ef34a59808c21d691d57
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-12 12:04:14 -0300

    up

--- lista01-diego/ex14.js
+++ lista01-diego/ex14.js
@@ -1,1 +1,16 @@
 const input = require('readline-sync')
+
+function reverter(array) {
+    let novoArray = [];
+
+    for (let i = array.length - 1; i >= 0; i--) {
+        novoArray.push(array[i]);
+    }
+
+    return novoArray;
+}
+
+const original = [1, 2, 3, 4, 5];
+const invertido = reverter(original);
+
+console.log(invertido);
