3a65e60dbaaea5ca48142816565ca99835c4bc2f
Millyss2 <emilly.oliveira18@escola.pr.gov.br>
2026-06-18 08:25:25 -0300

    atividade

--- aula02/ex01.js
+++ aula02/ex01.js
@@ -3,7 +3,7 @@ const input = require('readline-sync')
 let soma = 0
 
 let num1 = Number(input.question("Digite um numero aqui: "))
-let num2 = Number(input.question("Digite um numero aqui: "))
+let num2 = Number(input.question("Digite outro numero aqui: "))
 
 function somar(a, b){
     return a + b;
@@ -24,4 +24,12 @@ function divisao(a, b){
     return a / b;
 }
 
+console.clear()
+
+console.log("As quatro operações com os seus numeros ficam assim: ")
+
 console.log(somar(num1, num2))
+console.log(subtraçao(num1, num2))
+console.log(multiplicaçao(num1, num2))
+console.log(divisao(num1, num2))
+
